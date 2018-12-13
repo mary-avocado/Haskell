54b5245c2f7eeb83c6dd0c2e4ca19bf529a46083
Maria Mukhina <m.avocoder@gmail.com>
2018-12-13 22:34:20 +0300

    Lists, map, filter

--- Core.hs
+++ Core.hs
@@ -1,5 +1,6 @@
 module Core where
 import Prelude (Show)
+import qualified Prelude
 data Bool = 
   False | True
   deriving Show
@@ -32,7 +33,12 @@ notnot =
         True -> True
         False -> False
 data Nat = Zero | Succ Nat
-  deriving Show
+natToInt :: Nat -> Prelude.Int
+natToInt = \n -> case n of
+  Zero -> 0
+  Succ a -> 1 Prelude.+ natToInt a
+instance Prelude.Show Nat where
+  show = \n -> Prelude.show (natToInt n)
 (+) :: Nat -> Nat -> Nat
 (+) = 
   \x y ->
@@ -63,6 +69,59 @@ tail :: Stream -> Stream
 tail =
   \s ->
     case s of
-      (Neck x xs) -> xs  
+      (Neck x xs) -> xs
+nats :: Stream
+nats =
+  natsFrom Zero
+natsFrom :: Nat -> Stream
+natsFrom = \n ->
+  Neck n (natsFrom (Succ n))
+mapStream :: (Nat -> Nat) -> (Stream -> Stream)
+mapStream =
+  \f s ->
+    Neck (f (head s)) (mapStream f (tail s))
+filterStream :: (Nat -> Bool) -> (Stream -> Stream)
+filterStream = 
+  \p s ->
+    case s of
+      (Neck x xs) ->
+        case p x of
+          True -> (Neck x (filterStream p xs)) 
+          False -> filterStream p xs
+data List =
+  Nil | Cons Nat List
+  deriving Show
+  
+one = Succ Zero
+two = Succ one
+three = Succ two
+four = Succ three
+five =   Succ four
+six = Succ five
+seven = Succ six 
+  
+favnums =
+ Cons four (Cons five (Cons seven (Nil)))
+mapList :: (Nat -> Nat) -> (List -> List)
+mapList =
+  \n l ->
+     case l of
+       (Cons x xs) -> Cons (n x) (mapList n xs)
+       Nil -> Nil
+filterList ::(Nat -> Bool) -> (List -> List)
+filterList =
+  \p l ->
+    case l of
+      (Cons x xs) ->
+         case p x of
+           True -> (Cons x (filterList p xs))
+           False -> filterList p xs
+      Nil -> Nil
       
+           
+    
+
+
+
+
 
